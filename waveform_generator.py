80c566a13b19b4f8b7b5e455457da80283a68a2d
julian224 <36875273+julian224@users.noreply.github.com>
2018-02-26 21:53:49 -0800

    Add files via upload

---
+++ waveform_generator.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+dr = int(1e9)
+fs = int(10e9)
+
+Ts = 1/fs
+Td = 1/dr
+
+data = "10101010101100110011001110111011101110"
+bits = len(data)
+data_v = ""
+for i in range(len(data)):
+    data_v = data_v + data[i]*(int(fs/dr))
+
+data_v = (i for i in data_v)
+
+data_t = bits*int(fs/dr)
+data_t = (i*(1/fs) for i in range(data_t))
+
+plt.plot(list(data_t), list(data_v))
+plt.show()
